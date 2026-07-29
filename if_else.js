c2cb99461d73d03db6ed8dd6f90a3ccc99066266
carlitos-bit <carlosjapajunior@gmail.com>
2026-07-29 15:07:21 -0300

    .

---
+++ if_else.js
@@ -0,0 +1,8 @@
+let diaDaSemana = "quarta-feira";
+
+if(diaDaSemana === "sábado" || diaDaSemana === "domingo") {
+    console.log("É fim de semana!");
+} else {
+    console.log("É dia de semana.");
+}
+
